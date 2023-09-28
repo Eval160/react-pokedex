5cdcec26c4f15b8c572d8f02b72ec897638d78fc
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2023-09-28 15:35:09 +0200

    condition

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,30 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[1]
+
   return (
     <>
       <figure className="card">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-          className="card-img"
-        />
+        {
+         pokemon.imgSrc ?
+          <img
+            src={pokemon.imgSrc}
+            alt={pokemon.name}
+            className="card-img"
+          /> :
+          <p>???</p>
+
+        }
         <figcaption>bulbasaur</figcaption>
       </figure>
     </>
