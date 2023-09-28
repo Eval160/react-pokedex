29af7ec70847626f8a62479051b05986e6a7a17b
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2023-09-28 15:42:42 +0200

    pass prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import PokemonCard from "./components/PokemonCard"
 import "./App.css";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const pokemon = pokemonList[1]
+
   return (
     <div>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon}/>
     </div>
   );
 }
