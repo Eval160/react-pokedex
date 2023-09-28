63aa33e5b7215ea98273c1996dfe12e068d829e5
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2023-09-28 17:45:54 +0200

    state pokemonindex

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,64 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard"
 import "./App.css";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const pokemon = pokemonList[0]
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+  const pokemon = pokemonList[pokemonIndex]
+
+  const decrementPokemonIndex = () => {
+    if (pokemonIndex === 0) return
+    setPokemonIndex(pokemonIndex - 1)
+    console.log(pokemonIndex);
+  }
+
+  const incrementPokemonIndex = () => {
+    if (pokemonIndex >= pokemonList.length -1) return
+
+    setPokemonIndex(pokemonIndex + 1)
+    console.log(pokemonIndex);
+  }
 
   return (
     <div>
       <PokemonCard pokemon={pokemon}/>
+      <div>
+        {
+          pokemonIndex > 0 &&
+            <button onClick={decrementPokemonIndex}>Précédent</button>
+        }
+
+        {
+          pokemonIndex < pokemonList.length - 1&&
+            <button onClick={incrementPokemonIndex}>Suivant</button>
+        }
+      </div>
     </div>
   );
 }
